15875d1676d04c433175a51d470593e47f5c54b9
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-03-26 13:00:10 -0300

    Sincronizando coleções

--- src/App.java
+++ src/App.java
@@ -3,16 +3,16 @@ public class App {
 
         MeuRunnable runnable = new MeuRunnable();
 
-        Thread t0 = new Thread(runnable); // utiliza as funcionalidades do MeuRunnable
+        Thread t0 = new Thread(runnable);
         Thread t1 = new Thread(runnable);
         Thread t2 = new Thread(runnable);
-        Thread t3 = new Thread(runnable);
-        Thread t4 = new Thread(runnable);
 
         t0.start();
         t1.start();
         t2.start();
-        t3.start();
-        t4.start();
+
+        Thread.sleep(500);
+        System.out.println(MeuRunnable.getList());
+
     }
 }
--- src/MeuRunnable.java
+++ src/MeuRunnable.java
@@ -1,43 +1,19 @@
+import java.util.ArrayList;
+
 public class MeuRunnable implements Runnable {
 
-    static int i = -1;
-    static Object lock1 = new Object();
-    static Object lock2 = new Object();
+    private static ArrayList<String> lista = new ArrayList<>(); // listas tem problemas com trabalhar com threads!
+    // lista = Collections.synchronizedList(lista);
+    // utilizar a versão synchronized de acordo com o tipo0 de coleção (MAP, SET,
+    // LIST, COLLECTION)
 
-    // Se caso tivermos várias threads, somente uma por vez acessa o método com
-    // synchronized! Um de cada vez acessando, os outros esperam a thread atual
-    // terminar de usar o recurso (método)
-    /*
-     * public synchronized void run() { // Cada thread da instância runnable entra
-     * aqui
-     * i++; // mesmo recurso sendo compartilhado ao mesmo tempo!
-     * System.out.println(Thread.currentThread().getName() + " " + i);
-     * }
-     */
-
-    /*
-     * // Aqui eu acabo com o paralelismo
-     * public void run() {
-     * 
-     * synchronized (this) { // this é uma instância de objeto
-     * i++; // mesmo recurso sendo compartilhado ao mesmo tempo!
-     * System.out.println(Thread.currentThread().getName() + " " + i);
-     * }
-     * 
-     * synchronized (this) {
-     * i++; // mesmo recurso sendo compartilhado ao mesmo tempo!
-     * System.out.println(Thread.currentThread().getName() + " " + i);
-     * }
-     * }
-     */
-
-    // uso consciente
     public void run() {
-        int j;
-        synchronized (this) { // o recurso a ser concorrido fica no synchronized!
-            i++;
-            j = i * 2;
-        }
-        System.out.println(j);
+        lista.add("Inserido!");
+        System.out.println(Thread.currentThread().getName());
+
+    }
+
+    public static ArrayList<String> getList() {
+        return lista;
     }
 }
