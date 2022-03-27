d38f66c2f6e11f6170aadb0fa31a6d9af4688499
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-03-27 11:00:22 -0300

    Utilizando LOCK para resolver o problema de deadlock

--- src/App.java
+++ src/App.java
@@ -1,15 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-// Dedlock, race condition
+// Dedlock, race condition, região crítica (onde existe concorrência), mutex (LOCK ou synchronized)
+/* Race condition: situação de mais de uma tarefa atrapalhando a outra
+Deadlock: uma tarefa esperando por outra tareaf
+Sessão crítica: recursos sendo concorridos
+Mutex: isolamento das regiões críticas para uma thread em si, nunca mais de uma thread acessa a região crítica*/
 
 public class App {
 
-    private static final List<Integer> LISTA = new ArrayList<>(5);
+    private static final BlockingQueue<Integer> FILA = new LinkedBlockingDeque<>(5); // Fila com recursos de sincronismo
+                                                                                     // e mutex
     private static boolean produzindo = true;
     private static boolean consumindo = true;
+    private static final Lock LOCK = new ReentrantLock();
 
     public static void main(String[] args) throws Exception {
 
@@ -19,19 +28,24 @@ public class App {
                     processamento();
 
                     if (produzindo) {
+                        LOCK.lock(); // o lock possibilita que cada thread acesse sua condição e impossibilita que
+                                     // outra também entre
+
                         System.out.println("Produzindo");
                         int numero = new Random().nextInt(10000);
-                        LISTA.add(numero);
+                        FILA.add(numero);
 
-                        if (LISTA.size() == 5) {
+                        if (FILA.size() == 5) {
                             System.out.println("Pausa produtor");
                             produzindo = false;
                         }
 
-                        if (LISTA.size() == 1) {
+                        if (FILA.size() == 1) {
                             System.out.println("inicia consumidor");
                             consumindo = true;
                         }
+
+                        LOCK.unlock();
                     } else {
                         System.out.println("??? Produtor dormindo");
                     }
@@ -49,21 +63,25 @@ public class App {
                     processamento();
 
                     if (consumindo) {
+                        LOCK.lock();
+
                         System.out.println("Consumindo");
-                        Optional<Integer> numero = LISTA.stream().findFirst();
+                        Optional<Integer> numero = FILA.stream().findFirst();
                         numero.ifPresent(n -> {
-                            LISTA.remove(n);
+                            FILA.remove(n);
                         });
 
-                        if (LISTA.size() == 0) {
+                        if (FILA.size() == 0) {
                             System.out.println("Pausa consumidor");
                             consumindo = false; // pausa consumidor
                         }
 
-                        if (LISTA.size() == 4) {
+                        if (FILA.size() == 4) {
                             System.out.println("Inicia produtor");
                             produzindo = true; // inicia produtor
                         }
+
+                        LOCK.unlock();
                     } else {
                         System.out.println("??? Consumidor dormindo");
                     }
@@ -78,7 +96,7 @@ public class App {
     }
 
     private static final void processamento() {
-        int tempo = new Random().nextInt(40);
+        int tempo = new Random().nextInt(100);
         try {
             Thread.sleep(tempo);
         } catch (Exception e) {
