9a831cebba28e66cc577c0d440dc1ab1f5b00596
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-03-27 16:31:06 -0300

    Utilizando blockingQueue e scheduledThreadPool para prod/cons

--- src/App.java
+++ src/App.java
@@ -2,7 +2,10 @@
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -16,83 +19,44 @@ public class App {
 
     private static final BlockingQueue<Integer> FILA = new LinkedBlockingDeque<>(5); // Fila com recursos de sincronismo
                                                                                      // e mutex
-    private static boolean produzindo = true;
-    private static boolean consumindo = true;
-    private static final Lock LOCK = new ReentrantLock();
 
     public static void main(String[] args) throws Exception {
 
-        Thread produtor = new Thread(() -> {
-            while (true) {
-                try {
-                    processamento();
+        Runnable produtor = (() -> {
+            try {
+                processamento();
 
-                    if (produzindo) {
-                        LOCK.lock(); // o lock possibilita que cada thread acesse sua condição e impossibilita que
-                                     // outra também entre
+                System.out.println("Produzindo");
+                int numero = new Random().nextInt(10000);
+                FILA.put(numero); // coloca elemento na fila, se estiver cheia ele espera até liberar espaço
 
-                        System.out.println("Produzindo");
-                        int numero = new Random().nextInt(10000);
-                        FILA.add(numero);
-
-                        if (FILA.size() == 5) {
-                            System.out.println("Pausa produtor");
-                            produzindo = false;
-                        }
-
-                        if (FILA.size() == 1) {
-                            System.out.println("inicia consumidor");
-                            consumindo = true;
-                        }
-
-                        LOCK.unlock();
-                    } else {
-                        System.out.println("??? Produtor dormindo");
-                    }
-
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e.getMessage());
             }
+
         });
 
-        Thread consumidor = new Thread(() -> {
-            while (true) {
-                try {
-                    processamento();
+        Runnable consumidor = (() -> {
+            try {
+                processamento();
 
-                    if (consumindo) {
-                        LOCK.lock();
+                System.out.println("Consumindo");
+                FILA.take(); // retira elemento da fila, se estiver vazia espera até aparecer algum
 
-                        System.out.println("Consumindo");
-                        Optional<Integer> numero = FILA.stream().findFirst();
-                        numero.ifPresent(n -> {
-                            FILA.remove(n);
-                        });
-
-                        if (FILA.size() == 0) {
-                            System.out.println("Pausa consumidor");
-                            consumindo = false; // pausa consumidor
-                        }
-
-                        if (FILA.size() == 4) {
-                            System.out.println("Inicia produtor");
-                            produzindo = true; // inicia produtor
-                        }
-
-                        LOCK.unlock();
-                    } else {
-                        System.out.println("??? Consumidor dormindo");
-                    }
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e.getMessage());
             }
+
         });
 
-        produtor.start();
-        consumidor.start();
+        // Adicionado um executor de threads
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+
+        executor.scheduleWithFixedDelay(produtor, 0, 10, TimeUnit.MILLISECONDS);
+        executor.scheduleWithFixedDelay(consumidor, 0, 10, TimeUnit.MILLISECONDS);
+
     }
 
     private static final void processamento() {
